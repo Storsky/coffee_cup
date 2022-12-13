f7ca43252d5389e5d86cae8d09410fb1df2ca6fa
Storsky <andrkvasn@yandex.ru>
2022-12-13 12:31:09 +0400

    creators page

--- social_net/creators/urls.py
+++ social_net/creators/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index),
+    path('creators', show_all_profiles),
 
 
 ]
--- social_net/creators/views.py
+++ social_net/creators/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Profile
 
 def index(request):
-    pass
-    return None
+    template = 'base.html'
+    context = {}
+    return render(request, template, context)
+
+def show_all_profiles(request):
+    template = 'creators.html'
+    
+    context = {'profiles': Profile.objects.exclude(user=request.user)}
+    return render(request, template, context)
